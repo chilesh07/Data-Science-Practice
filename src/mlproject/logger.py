0f5010217ac823d7868f1273bc82edff36d9365d
tschilesh7 <tschilesh7@gmail.com>
2026-01-13 16:03:11 +0530

    data train_test_split

---
+++ src/mlproject/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+# Log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Log directory
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Full log file path
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# Export logger
+logger = logging.getLogger()
